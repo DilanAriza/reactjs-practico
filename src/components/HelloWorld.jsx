2463e78cfb4e0a964995b5e198d2a865c66d3c66
Dilan Ariza <dilansantiagoariza@gmail.com>
2020-09-28 14:31:49 -0500

    Eslint

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './../assets/styles/App.scss';
+
+// export default function HelloWorld() {
+//     return (
+//         <div>
+//             <h1>Hola mundo</h1>
+//         </div>
+//     )
+// }
+
+
+const HelloWorld = () => (
+    <h1>Hola ptos</h1>
+)
+
+export default HelloWorld
